5b6232753cdecfb0a0628ab4607bee052fc85ef5
Banana <bananachristian04@proton.me>
2026-01-08 22:30:32 +0300

    Fixed null system

--- include/irgen.hpp
+++ include/irgen.hpp
@@ -142,8 +142,8 @@ private:
   llvm::Value *generateU128Literal(Node *node);
   llvm::Value *generateISIZELiteral(Node *node);
   llvm::Value *generateUSIZELiteral(Node *node);
+  llvm::Value *generateNullLiteral(Node *node);
 
-  llvm::Value *generateNullLiteral(NullLiteral *nullLit, DataType type);
   llvm::Value *generateIdentifierExpression(Node *node);
   llvm::Value *generateAddressExpression(Node *node);
   llvm::Value *generateDereferenceExpression(Node *node);
