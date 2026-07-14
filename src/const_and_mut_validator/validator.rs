2d063dadb5196785fc1b61388d30a7679720ec27
BananaChristian <bananachristian04@proton.me>
2026-07-14 16:40:39 +0300

    Comtinued on the validator

--- src/const_and_mut_validator/validator.rs
+++ src/const_and_mut_validator/validator.rs
@@ -1,7 +1,8 @@
 use std::collections::HashMap;
 
 use crate::{
-    hir::{HirExpr, HirExprKind, HirStmt, HirStmtKind},
+    diagnostics::{self, CompilerError, Diagnostics, Phase, Span},
+    hir::{HirExpr, HirExprKind, HirLiteral, HirStmt, HirStmtKind},
     indexer::NodeIndex,
     semantics::SemanticCtxt,
 };
@@ -16,15 +17,21 @@ enum BindingKind {
 pub struct Validator<'a> {
     node_index: &'a NodeIndex,
     ctxt: &'a SemanticCtxt,
+    diagnostics: &'a mut Diagnostics,
     scopes: Vec<HashMap<String, BindingKind>>,
     pub corrupted: bool,
 }
 
 impl<'a> Validator<'a> {
-    pub fn new(node_index: &'a NodeIndex, ctxt: &'a SemanticCtxt) -> Self {
+    pub fn new(
+        node_index: &'a NodeIndex,
+        ctxt: &'a SemanticCtxt,
+        diagnostics: &'a mut Diagnostics,
+    ) -> Self {
         Validator {
             node_index,
             ctxt,
+            diagnostics,
             scopes: vec![HashMap::new()],
             corrupted: false,
         }
@@ -49,22 +56,84 @@ impl<'a> Validator<'a> {
 
     fn is_compile_time_literal(&self, expr: &HirExpr) -> bool {
         match &expr.kind {
-            HirExprKind::Literal(_) => true,
-            // If it's a complex expression (like an addition or function call), it's not a raw literal
+            HirExprKind::Literal(inner) => match inner {
+                HirLiteral::Null | HirLiteral::ArrayLiteral(_) => false, //For now these are a no
+                //go
+                _ => true,
+            },
             _ => false,
         }
     }
 
-    fn check_var_decl(&self, stmt: &HirStmt) {
+    fn run(&mut self) {
+        for (_,stmt) in &self.node_index.nodes{
+            self.check_stmt(stmt);
+        }
+
+    }
+
+    fn check_stmt(&mut self, stmt: &HirStmt){
+        
+    }
+
+    fn check_var_decl(&mut self, stmt: &HirStmt) {
         if let HirStmtKind::HirVarDecl {
             name,
             mutable,
             constant,
-            heap,
-            exposed,
-            ty,
             init,
+            ..
         } = &stmt.kind
-        {}
+        {
+            let is_constant = *constant;
+            let is_mutable = *mutable;
+
+            //Mut and const are mutually exclusive
+            if is_mutable && is_constant {
+                self.report(
+                    format!(
+                        "Variable '{}' cannot be const and mutable at the same time",
+                        name,
+                    ),
+                    Some(stmt.span.clone()),
+                );
+            }
+
+            //Is it constant if so, apply come checks
+            if is_constant {
+                if let Some(initializer) = init {
+                    if !self.is_compile_time_literal(initializer) {
+                        self.report(format!("Constant variable '{}' must be initilialized with a compile time value",name), 
+                            Some(stmt.span.clone()));
+                    }
+                } else {
+                    self.report(
+                        format!("Constant variable '{}' must be initialized", name),
+                        Some(stmt.span.clone()),
+                    );
+                }
+
+                self.scopes
+                    .last_mut()
+                    .unwrap()
+                    .insert(name.clone(), BindingKind::Const);
+            } else if is_mutable {
+                self.scopes
+                    .last_mut()
+                    .unwrap()
+                    .insert(name.clone(), BindingKind::Mutable);
+            } else {
+                self.scopes
+                    .last_mut()
+                    .unwrap()
+                    .insert(name.clone(), BindingKind::Immutable);
+            }
+        }
+    }
+
+    pub fn report(&mut self, message: String, span: Option<Span>) {
+        self.corrupted = true;
+        self.diagnostics
+            .report(CompilerError::error(message, Phase::Semantics, span));
     }
 }
